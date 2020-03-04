a9c501fe3c53e382d8b92ae47203a06c34e0a768
licip <licip@163.com>
2020-03-04 11:50:58 +0800

    fff

--- src/main/java/com/etc/test/Test.java
+++ src/main/java/com/etc/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("this is my idea! git");
+        System.out.println("this is my oracle");
+        System.out.println("ddjsfjjkdfjdf");
     }
 }
