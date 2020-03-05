9f19ade790c7cfab66239ffcce0752addfc26c4d
licip <licip@163.com>
2020-03-05 09:23:32 +0800

    20200305写了打印输出hello

--- src/main/java/com/etc/test/Test.java
+++ src/main/java/com/etc/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("this is my idea! git");
         System.out.println("this is my oracle");
+        System.out.println("hello");
     }
 }
